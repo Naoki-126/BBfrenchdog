cf9cd57d93046d9b7a8ec42ded160e8c5efbcc9f
Naoking <naokiyokota.126@gmail.com>
2025-07-21 13:08:12 +0900

    edit:一覧ページ実装

--- assets/custom.js
+++ assets/custom.js
@@ -1,1 +1,11 @@
 // alert("テスト");
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleBtn = document.getElementById('js-toggle-sort');
+    const sortOptions = document.getElementById('js-sort-options');
+
+    toggleBtn.addEventListener('click', function () {
+      sortOptions.classList.toggle('open');
+      toggleBtn.classList.toggle('active');
+    });
+  });
