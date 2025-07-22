354626a50ee294239329b8cc34deb99d93cc0443
Naoking <naokiyokota.126@gmail.com>
2025-07-23 07:07:29 +0900

    edit：faqセクション実装

--- assets/custom.js
+++ assets/custom.js
@@ -10,4 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
-  
+document.getElementById('js-faq').addEventListener('click', function (e) {
+  const clicked = e.target;
+
+  if (clicked.classList.contains('accordion-q')) {
+    const answer = clicked.nextElementSibling;
+
+    // 回答表示切り替え
+    answer.classList.toggle('active');
+
+    // 矢印反転用クラス切り替え
+    clicked.classList.toggle('active');
+  }
+});
+
